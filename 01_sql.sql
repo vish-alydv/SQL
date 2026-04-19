dd2e67193073f0a54d9643d9b4908d9687370aab
Vishal <thevishaleditor@gmail.com>
2026-04-19 16:45:17 +0530

    Add files via upload

---
+++ 01_sql.sql
@@ -0,0 +1,12 @@
+-- Create table numbers(
+-- Id serial,
+-- Age smallint,
+-- Price numeric(4,2),
+-- Rating real
+-- );
+
+insert into numbers (Age,Price,Rating)
+values (24,22.66,45.67) , 
+(34,);
+
+select * from numbers
