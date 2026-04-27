594f6d2e61252f9c11b5275ebb398f7bf7976f29
Vishal <thevishaleditor@gmail.com>
2026-04-27 18:55:08 +0530

    Sql

---
+++ 11.sql
@@ -0,0 +1,18 @@
+select * from products;
+
+select name,price,
+Case when (price>1000) then 'Expensive'
+when price between 500 and 1000 then 'Moderate'
+Else 'Cheap'
+End as price_tag From products;
+
+select * from products;
+
+
+alter table products add column price_tag text;
+
+update products set price_tag = 
+case when (price>1000) then 'Expensive'
+when price between 500 and 1000 then 'Moderate'
+Else 'Cheap'
+end;
